5e4f18acc07aa262f9bfef24b3882b2268cb8e37
khaled-hamdi <96597490+khaled-hamdi@users.noreply.github.com>
2026-08-15 01:09:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -397,7 +397,7 @@ def main():
             
             st.dataframe(df_summary, hide_index=True, use_container_width=True)
 
-        else:
+            else:
             # --- CITY PAGES ---
             df, col_map, err = load_data(CITIES_DATA[city]['file'])
             if err: 
