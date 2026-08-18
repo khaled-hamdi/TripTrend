d1695c9bb4c08debb540a7d18e49a9ed8078dbde
khaled-hamdi <96597490+khaled-hamdi@users.noreply.github.com>
2026-08-18 23:59:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def update_last_login(username):
 # --- DATA CONFIG ---
 # ======================================================================================
 CITIES_DATA = {
-    "Paris": {"file": "Paris.xlsx", "emoji": "🗼"},
+    "Paris": {"file": "paris.xlsx", "emoji": "🗼"},
     "Dubai": {"file": "Dubai.xlsx", "emoji": "🏙️"},
     "Istanbul": {"file": "istanbul_hotels.xlsx", "emoji": "🕌"},
     "NewYork": {"file": "NewYork.xlsx", "emoji": "🏛️"}
