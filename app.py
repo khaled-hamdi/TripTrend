b066e79c60338189cf888d15e80e9473a9c1ce66
khaled-hamdi <96597490+khaled-hamdi@users.noreply.github.com>
2026-08-21 02:20:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ def try_parse_dates(series):
 @st.cache_data
 def load_data(city_name):
     file_path = smart_find_file(city_name)
-    if not file_path: return None, None, f"Excel file for {city_name} not found."
+    if not file_path: return None, None, f"Error file for {city_name} not found."
     try:
         df = pd.read_excel(file_path)
         df.columns = df.columns.str.strip()
